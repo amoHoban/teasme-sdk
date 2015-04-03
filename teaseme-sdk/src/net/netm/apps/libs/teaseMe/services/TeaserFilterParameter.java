3bad2f6e3dcbd8b119ad774d68873e869a8b7145
ahoban <amoloco@gmail.com>
2015-04-03 13:54:00 +0200

    restructure, refactor, use picasso for images

---
+++ teaseme-sdk/src/net/netm/apps/libs/teaseMe/services/TeaserFilterParameter.java
@@ -0,0 +1,7 @@
+package net.netm.apps.libs.teaseMe.services;
+
+/**
+ * Created by ahoban on 01.04.15.
+ */
+public class TeaserFilterParameter {
+}
---
+++ teaseme-sdk/src/net/netm/apps/libs/teaseMe/utils/transformations/CircleTransformation.java
@@ -0,0 +1,7 @@
+package net.netm.apps.libs.teaseMe.utils.transformations;
+
+/**
+ * Created by ahoban on 01.04.15.
+ */
+public class CircleTransformation {
+}
